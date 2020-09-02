bf1ccdf985e3f3cab893589f3ba446061ef6ac4a
n0hack <nohack-@naver.com>
2020-09-02 21:53:14 +0900

    add new layout

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express'),
     ejs = require('ejs');
+
 var app = express();
 
 // view 미들웨어 연결
@@ -7,61 +8,15 @@ app.set('views', 'views');
 app.set('view engine', 'ejs');
 
 // 라우팅 등록
-app.get('/', function(req, res){    
-    res.render('layout.ejs', { data: 'index' });    
-});
-
-app.get('/story/astera', function(req, res){ 
-    res.render('layout.ejs', { data: 'astera' });    
-});
-
-app.get('/story/rule', function(req, res){  
-    res.render('layout.ejs', { data: 'rule' });   
-});
-
-app.get('/story/career', function(req, res){    
-    res.render('layout.ejs', { data: 'career' });   
-});
-
-app.get('/content/chat', function(req, res){   
-    res.render('layout.ejs', { data: 'chat' });
-});
-
-app.get('/content/raid', function(req, res){
-    res.render('layout.ejs', { data: 'raid' });
-});
-
-app.get('/content/guide', function(req, res){
-    res.render('layout.ejs', { data: 'guide' });
-});
-
-app.get('/content/lol', function(req, res){
-    res.render('layout.ejs', { data: 'lol' });
-});
-
-app.get('/download/game', function(req, res){
-    res.render('layout.ejs', { data: 'game' });
-});
-
-app.get('/download/util', function(req, res){
-    res.render('layout.ejs', { data: 'util' });
-});
-
-app.get('/cs/question', function(req, res){
-    res.render('layout.ejs', { data: 'question' });
-});
-
-app.get('/cs/report', function(req, res){
-    res.render('layout.ejs', { data: 'report' });
-});
-
-app.get('/manage/log', function(req, res){
-    res.render('layout.ejs', { data: 'log' });
-});
-
-app.get('/manage/sponsor', function(req, res){
-    res.render('layout.ejs', { data: 'sponsor' });
-});
+app.get('/', function(req, res){ res.render('layout.ejs', { data: 'index' }); });
+app.get('/story/astera', function(req, res){  res.render('layout.ejs', { data: 'astera' }); });
+app.get('/story/rule', function(req, res){ res.render('layout.ejs', { data: 'rule' }); });
+app.get('/story/career', function(req, res){ res.render('layout.ejs', { data: 'career' }); });
+app.get('/content/chat', function(req, res){ res.render('layout.ejs', { data: 'chat' }); });
+app.get('/content/lol', function(req, res){ res.render('layout.ejs', { data: 'lol' }); });
+app.get('/product/app', function(req, res){ res.render('layout.ejs', { data: 'app' }); });
+app.get('/cs/support', function(req, res){ res.render('layout.ejs', { data: 'support' }); });
+app.get('/manage/sponsor', function(req, res){ res.render('layout.ejs', { data: 'sponsor' }); });
 
 // public static 연결
 app.use('/', express.static(__dirname + '/public'));
@@ -70,13 +25,8 @@ app.use('/story/rule', express.static(__dirname + '/public'));
 app.use('/story/career', express.static(__dirname + '/public'));
 app.use('/content/chat', express.static(__dirname + '/public'));
 app.use('/content/raid', express.static(__dirname + '/public'));
-app.use('/content/guide', express.static(__dirname + '/public'));
-app.use('/content/lol', express.static(__dirname + '/public'));
-app.use('/download/game', express.static(__dirname + '/public'));
-app.use('/download/util', express.static(__dirname + '/public'));
-app.use('/cs/question', express.static(__dirname + '/public'));
-app.use('/cs/report', express.static(__dirname + '/public'));
-app.use('/manage/log', express.static(__dirname + '/public'));
+app.use('/product/app', express.static(__dirname + '/public'));
+app.use('/cs/support', express.static(__dirname + '/public'));
 app.use('/manage/sponsor', express.static(__dirname + '/public'));
 
 // 서버 연결
